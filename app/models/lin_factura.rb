14784db41b811cb0a24935fdf3096f5068002a4f
SergioMedeirosGarcia <smedeiros@aspgems.com>
2019-02-27 12:16:14 +0100

    new model added

--- app/models/lin_factura.rb
+++ app/models/lin_factura.rb
@@ -1,3 +1,4 @@
 class LinFactura < ApplicationRecord
 	belongs_to :factura, :foreign_key => :NumFactura, :primary_key => :id
+	belongs_to :producto, :foreign_key => :Concepto , :primary_key => :id
 end
